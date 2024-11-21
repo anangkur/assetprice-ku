1c2720a696d14efc97aacdf7bed0c83956f68e6c
Anang Kurniawan <anangk97@gmail.com>
2024-11-21 13:23:35 +0700

    Initial Commit

---
+++ coinpriceku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coinpriceku
+//
+//  Created by Anang Kurniawan on 21/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ coinpricekuTests/coinpricekuTests.swift
@@ -0,0 +1,17 @@
+//
+//  coinpricekuTests.swift
+//  coinpricekuTests
+//
+//  Created by Anang Kurniawan on 21/11/24.
+//
+
+import Testing
+@testable import coinpriceku
+
+struct coinpricekuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
